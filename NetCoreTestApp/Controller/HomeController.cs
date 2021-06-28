4c8b27b758e53cf76a1e73f1ae6c43136cd67a7b
bilal-comsian <bilal.comsian09@gmail.com>
2021-06-29 00:35:29 +0500

    Project added

---
+++ NetCoreTestApp/Controller/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreTestApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/home
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
